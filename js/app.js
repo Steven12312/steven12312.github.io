78e797abf5d4b38bf6860bf1c32157499e67187a
Steven12312 <steventchanra@hotmail.com>
2021-11-04 18:38:22 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 
-  function getUserMedia(constraints) {
+  /*function getUserMedia(constraints) {
     // if Promise-based API is available, use it
     if (navigator.mediaDevices) {
       return navigator.mediaDevices.getUserMedia(constraints);
@@ -63,6 +63,62 @@ if ("serviceWorker" in navigator) {
       .catch(function (err) {
         alert('Error: ' + err);
       });
+
       
+  }*/
+  function getUserMedia(options, successCallback, failureCallback) {
+    var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    if (api) {
+      return api.bind(navigator)(options, successCallback, failureCallback);
+    }
   }
+  
+  var theStream;
+  
+  function getStream() {
+    if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+      !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+      alert('User Media API not supported.');
+      return;
+    }
+    
+    var constraints = {
+      video: true
+    };
+  
+    getUserMedia(constraints, function (stream) {
+      var mediaControl = document.querySelector('video');
+      if ('srcObject' in mediaControl) {
+        mediaControl.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+        mediaControl.mozSrcObject = stream;
+      } else {
+        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+      }
+      theStream = stream;
+    }, function (err) {
+      alert('Error: ' + err);
+    });
+  }
+  
+  function takePhoto() {
+    if (!('ImageCapture' in window)) {
+      alert('ImageCapture is not available');
+      return;
+    }
+    
+    if (!theStream) {
+      alert('Grab the video stream first!');
+      return;
+    }
+    
+    var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+  
+    theImageCapturer.takePhoto()
+      .then(blob => {
+        var theImageTag = document.getElementById("imageTag");
+        theImageTag.src = URL.createObjectURL(blob);
+      })
+      .catch(err => alert('Error: ' + err));}
 }
