c70ea2e7a2cb2c16f09fa0a005da266b08fc5de0
Steven12312 <steventchanra@hotmail.com>
2021-11-11 16:06:57 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,25 @@ if ("serviceWorker" in navigator) {
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
+  if ('DeviceOrientationEvent' in window) {
+    window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+  } else {
+    document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
+  }
+  
+  function deviceOrientationHandler (eventData) {
+    var tiltLR = eventData.gamma;
+    var tiltFB = eventData.beta;
+    var dir = eventData.alpha;
+    
+    document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+    document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+    document.getElementById("doDirection").innerHTML = Math.round(dir);
+  
+    var logo = document.getElementById("imgLogo");
+    logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+    logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
+    logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)"; }
 
    //Location
    var target = document.getElementById('target');
@@ -21,7 +40,7 @@ if ("serviceWorker" in navigator) {
      target.appendChild(newLocation);
    }
    
-   if ('geolocation' in navigator) {
+  /* if ('geolocation' in navigator) {
      document.getElementById('askButton').addEventListener('click', function () {
        navigator.geolocation.getCurrentPosition(function (location) {
          appendLocation(location, 'fetched');
@@ -122,5 +141,5 @@ if ("serviceWorker" in navigator) {
       return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
     }
     
-    var base64 = getBase64Image(document.getElementById("imageTag"));  
+    var base64 = getBase64Image(document.getElementById("imageTag")); */ 
 }
