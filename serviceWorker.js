206f0621184299e3e5411ac8505a495be56d6519
Steven12312 <steventchanra@hotmail.com>
2021-12-14 12:27:44 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,32 @@
-const staticDevCoffee = "dev-coffee-site-v1";
-const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
+var cacheName = 'Steven-PWA';
+var filesToCache = [
+  '/index.html',
+  'js/app.js'
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then(function (cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+      } )
+    );
+  });
+  
+  self.addEventListener('activate', function(e)
+  {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList){
+      return Promise.all(keyList.map(function(key){
+        if(key !== cacheName){
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
     })
-  );
-});
+  )
+  return self.clients.claim();
+  });
